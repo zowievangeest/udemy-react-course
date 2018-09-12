a265a1768bd1739b849624c38d1915472ab83714
Zowie van Geest <zowie@zowievangeest.nl>
2018-09-12 10:15:17 +0200

    feat: add oefening 5

--- oefeningen/oefening-5/src/components/Persons/Person/Person.js
+++ oefeningen/oefening-5/src/components/Persons/Person/Person.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import WithClass from '../../../hoc/WithClass';
+import PropTypes from 'prop-types';
+import Aux from '../../../hoc/Aux';
+import withClass from '../../../hoc/withClass';
 
 import classes from './Person.css';
 
@@ -15,18 +17,32 @@ class Person extends Component {
 
   componentDidMount() {
     console.log('[Person.js] inside ComponentDidMount');
+    if(this.props.position === 0) {
+      this.inputElement.focus();
+    }
   }
 
   render() {
     console.log('[Person.js] inside Render()');
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <p onClick={this.props.click}> i'm {this.props.name}! and I'm {this.props.age} years old.</p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}/>
-      </WithClass>
+        <input
+          ref={(inp) => { this.inputElement = inp }}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}/>
+      </Aux>
     )
   }
 }
 
-export default Person;
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
+
+export default withClass(Person, classes.Person);
--- oefeningen/oefening-5/src/containers/App.js
+++ oefeningen/oefening-5/src/containers/App.js
@@ -2,7 +2,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import Aux from '../hoc/Aux';
+import withClass from '../hoc/withClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -15,7 +16,8 @@ class App extends PureComponent {
         { id: '345', name: 'Vis', age: 18 },
       ],
       otherState: 'some other value',
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     }
   }
 
@@ -70,7 +72,12 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      }
+    });
   }
 
   render() {
@@ -86,7 +93,7 @@ class App extends PureComponent {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
           <Cockpit
             appTitle={this.props.title}
@@ -95,9 +102,9 @@ class App extends PureComponent {
             clicked={this.togglePersonsHandler}
             />
           {persons}
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
